0812e8d5b7a6027fa124133747fbf50e2ca08d95
renwuxun <axun9527>
2018-12-07 11:52:34 +0800

    允许使用未来的时间ms(修改格式)

--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 雪花ID生成器
 
 #### 第一种：snowflake_s
-ttttttttttttttttttttttttttttttttttttttttttmmmmmmmmmmssssssssssss
+mmmmmmmmmmttttttttttttttttttttttttttttttttttttttttttssssssssssss
 最多1024个进程，一个毫秒容量为4096
 
 #### 第二种：snowflake2_s
-ttttttttttttttttttttttttttttttttttttttttttmmmmmmmmssssssssssssss
+mmmmmmmmttttttttttttttttttttttttttttttttttttttttttssssssssssssss
 最多256个进程，一个毫秒容量为16383
 
-* t：毫秒时间戳，42位
 * m：全局进程id，10/8位
+* t：毫秒时间戳，42位
 * s：时间点计时器，12/14位
 
--- snowflake.c
+++ snowflake.c
@@ -1,7 +1,7 @@
 #include "snowflake.h"
 
 
-int snowflake_init(struct snowflake_s* gen, uint64_t gpid) {
+int snowflake_init(struct snowflake_s *gen, uint64_t gpid) {
     if (gpid > 1023) {
         return __LINE__;
     }
@@ -12,7 +12,7 @@ int snowflake_init(struct snowflake_s* gen, uint64_t gpid) {
     return 0;
 }
 
-int snowflake2_init(struct snowflake2_s* gen, uint64_t gpid) {
+int snowflake2_init(struct snowflake2_s *gen, uint64_t gpid) {
     if (gpid > 255) {
         return __LINE__;
     }
@@ -26,57 +26,57 @@ int snowflake2_init(struct snowflake2_s* gen, uint64_t gpid) {
 static inline uint64_t now_ms() {
     struct timeval t;
     gettimeofday(&t, 0);
-    return (uint64_t)(t.tv_sec*1000 + t.tv_usec/1000);
+    return (uint64_t) (t.tv_sec * 1000 + t.tv_usec / 1000);
 }
 
-uint64_t snowflake_nts_gen(struct snowflake_s* gen) {
+uint64_t snowflake_nts_gen(struct snowflake_s *gen) {
     uint64_t nowms = now_ms() - SNOWFLAKE_EPOCH;
 
-    if ((gen->statems >> 22) < nowms) {
-        gen->statems = nowms<<22;
+    if ((gen->statems >> 12) < nowms) {
+        gen->statems = nowms << 12;
     }
 
     gen->statems++;
     //gen->statems = gen->statems &~ 0x3fc000; // 清理gpid的位置
 
-    return gen->statems | gen->gpid<<12;
+    return gen->statems | gen->gpid << 56;
 }
 
-uint64_t snowflake2_nts_gen(struct snowflake2_s* gen) {
+uint64_t snowflake2_nts_gen(struct snowflake2_s *gen) {
     uint64_t nowms = now_ms() - SNOWFLAKE_EPOCH;
 
-    if ((gen->statems >> 22) < nowms) {
-        gen->statems = nowms<<22;
+    if ((gen->statems >> 14) < nowms) {
+        gen->statems = nowms << 14;
     }
 
     gen->statems++;
     //gen->statems = gen->statems &~ 0x3fc000; // 清理gpid的位置
 
-    return gen->statems | gen->gpid<<14;
+    return gen->statems | gen->gpid << 56;
 }
 
-uint64_t snowflake_gen(struct snowflake_s* gen) {
+uint64_t snowflake_gen(struct snowflake_s *gen) {
     uint64_t nowms = now_ms() - SNOWFLAKE_EPOCH;
 
-    if ((gen->statems >> 22) < nowms) {
-        gen->statems = nowms<<22;
+    if ((gen->statems >> 12) < nowms) {
+        gen->statems = nowms << 12;
     }
 
     __sync_fetch_and_add(&gen->statems, 1);
     //gen->statems = gen->statems &~ 0x3fc000; // 清理gpid的位置
 
-    return gen->statems | gen->gpid<<12;
+    return gen->statems | gen->gpid << 56;
 }
 
-uint64_t snowflake2_gen(struct snowflake2_s* gen) {
+uint64_t snowflake2_gen(struct snowflake2_s *gen) {
     uint64_t nowms = now_ms() - SNOWFLAKE_EPOCH;
 
-    if ((gen->statems >> 22) < nowms) {
-        gen->statems = nowms<<22;
+    if ((gen->statems >> 14) < nowms) {
+        gen->statems = nowms << 14;
     }
 
     __sync_fetch_and_add(&gen->statems, 1);
     //gen->statems = gen->statems &~ 0x3fc000; // 清理gpid的位置
 
-    return gen->statems | gen->gpid<<14;
+    return gen->statems | gen->gpid << 56;
 }
